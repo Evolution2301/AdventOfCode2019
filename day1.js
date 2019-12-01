c9d0bd417dafa821668428f61c06220e36f1301d
Christoph <christoph@moldenhauer.xyz>
2019-12-01 18:09:17 +0100

    First day

---
+++ day1.js
@@ -0,0 +1,45 @@
+var parse = require("./parseInput.js").parseNewlineInput;
+console.log("Day 1");
+
+function calculateFuel(mass) {
+    let temp = Math.floor(mass / 3);
+    //console.log(mass, temp, (temp-2));
+    return (temp - 2);
+}
+
+function day1Part1() {
+    let shipModules = parse("day1");
+    //console.log(shipModules);
+    let sum = 0;
+    for(var i in shipModules) {
+        sum += calculateFuel(Number(shipModules[i]));
+    }
+    console.log("Part1:", sum);
+}
+
+day1Part1();
+
+
+function calculateRequiredFuel(startingMass) {
+    let additionalFuel = calculateFuel(startingMass);
+    //console.log(startingMass, additionalFuel);
+    let mass = 0;
+    do {
+        mass += additionalFuel;
+        additionalFuel = calculateFuel(additionalFuel);
+    } while(additionalFuel > 0);
+    //console.log(startingMass, mass);
+    return mass;
+}
+
+function day1Part2() {
+    let shipModules = parse("day1");
+    //console.log(shipModules);
+    let sum = 0;
+    for(var i in shipModules) {
+        sum += calculateRequiredFuel(Number(shipModules[i]));
+    }
+    console.log("Part2:", sum);
+}
+
+day1Part2();
---
+++ parseInput.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let path = require('path');
+
+function getPath(fileName) {
+    return path.join(__dirname, 'inputs', fileName);
+}
+
+function readFile(fileName) {
+    return fs.readFileSync(getPath(fileName), { encoding: 'utf8' });
+}
+
+exports.parseNewlineInput = function(fileName) {
+    return readFile(fileName).split("\r\n");
+}
